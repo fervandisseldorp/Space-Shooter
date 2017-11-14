d0113d20924dc82c5979c0a5a27b5019ec6e683c
fervandisseldorp <fervandisseldorp@gmail.com>
2017-11-14 16:10:00 +0100

    push old local project to github

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+    Rigidbody rb;
+    public float tumble;
+
+	// Use this for initialization
+	void Start () {
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+
+}
